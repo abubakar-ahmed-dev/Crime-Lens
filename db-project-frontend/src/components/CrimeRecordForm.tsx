458e9152f70b6f43592720b41fabbc545cd6c145
abubakar-ahmed-dev <abubakar.ahmed.dev@gmail.com>
2026-07-04 00:08:15 +0500

    fix: change UI of popup form

--- db-project-frontend/src/components/CrimeRecordForm.tsx
+++ db-project-frontend/src/components/CrimeRecordForm.tsx
@@ -41,30 +41,16 @@ export default function CrimeRecordForm({
   const defaultMapCenter = isValidLocation(locationValue)
     ? locationValue
     : {
-        latitude: "24.899983520748542",
-        longitude: "67.05814361572267",
-      };
+      latitude: "24.899983520748542",
+      longitude: "67.05814361572267",
+    };
 
   return (
     <>
       <div className="border-b pb-4">
         <h3 className="font-semibold text-gray-800 mb-3">Crime Info</h3>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 mb-3">
-          <div className="bg-gray-50 p-3 rounded-lg">
-            <label className="text-xs font-medium text-gray-700">Zone</label>
-            <select
-              value={value.zoneId}
-              onChange={(e) => onChange("zoneId", Number(e.target.value))}
-              className="w-full text-sm bg-gray-100 mt-1 p-2 rounded border"
-            >
-              <option value="">Select zone</option>
-              {zones.map((zone) => (
-                <option key={zone.id} value={zone.id}>
-                  {zone.id} - {zone.name}
-                </option>
-              ))}
-            </select>
-          </div>
+
 
           <div className="bg-gray-50 p-3 rounded-lg">
             <label className="text-xs font-medium text-gray-700">Crime Type</label>
@@ -92,7 +78,22 @@ export default function CrimeRecordForm({
             />
           </div>
         </div>
-
+        
+        <div className="bg-gray-50 p-3 rounded-lg mb-3">
+          <label className="text-xs font-medium text-gray-700">Zone</label>
+          <select
+            value={value.zoneId}
+            onChange={(e) => onChange("zoneId", Number(e.target.value))}
+            className="w-full text-sm bg-gray-100 mt-1 p-2 rounded border"
+          >
+            <option value="">Select zone</option>
+            {zones.map((zone) => (
+              <option key={zone.id} value={zone.id}>
+                {zone.id} - {zone.name}
+              </option>
+            ))}
+          </select>
+        </div>
         <div className="bg-gray-50 p-3 rounded-lg mb-3">
           <label className="text-xs font-medium text-gray-700">Title</label>
           <textarea
